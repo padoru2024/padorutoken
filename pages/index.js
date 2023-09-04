0d4c310ad73f07af53b50263067161d08fc36149
elmoebuta <u201726329@upc.edu.pe>
2023-09-04 14:13:16 -0500

    first commit
    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,105 +1,73 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useTokenSupply,useContract,useAddress, useTokenBalance, useClaimToken } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
+import { Web3Button } from "@thirdweb-dev/react";
+import { useState } from "react";
 
 export default function Home() {
+  const contractAddress ="0xDD720873AC7F0eFcaa27a03A26710c7edbeDD50B";
+  const [amount, setAmount] = useState("");
+  const address =useAddress();
+  const {
+    contract
+  } = useContract(contractAddress);
+
+  const {
+    data: balance
+  } =useTokenBalance(contract,address);
+
+  const { data: supply } = useTokenSupply(contract);
+
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
+      <div className={styles.connect}>
             <ConnectWallet
               dropdownPosition={{
                 side: "bottom",
                 align: "center",
+                
               }}
-            />
+              
+            /><div>
+              PEPES DEL MUNDO: 
+              {supply?.displayValue}
+              {supply?.symbol}
+
+            </div>
+            <div> 
+              Tus PEPES: 
+              {balance?.displayValue}
+            {balance?.symbol}
+            </div>
+           
           </div>
+        <div className={styles.header}>
+          <h1 className={styles.title}>
+            Welcome to PepeLePew, your new best investment!
+            
+          </h1>
+          
+
+          <p className={styles.description}>
+            Assure your new investment with 50% extra list price!!
+          </p>
+
+          <p>Claim your PEPES:  </p>
+          <input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}/>
+          <Web3Button
+      contractAddress={contractAddress}
+      
+      action={(contract) => contract.erc20.claim(amount.toString())}
+    >
+      Claim {amount} {supply?.symbol}
+    </Web3Button>
+         
+
+          
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+        
       </div>
     </main>
   );
